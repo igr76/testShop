e3d0e34242edf99ab5ef276e7e39bf7de734fdc1
igr76 <gritsuk76@mail.ru>
2023-04-22 10:37:50 +1000

    process

---
+++ src/test/java/com/testShop/TestShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
